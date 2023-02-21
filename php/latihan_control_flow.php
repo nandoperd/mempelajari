bf9d0538138600b5a8b45e480b4f5124aea826c7
nandoperd <63334325+nandoperd@users.noreply.github.com>
2023-02-21 07:37:57 +0700

    Update latihan_control_flow.php

--- php/latihan_control_flow.php
+++ php/latihan_control_flow.php
@@ -23,3 +23,31 @@ $i=0;
         $i++;
     }while($i<3);
 ?>
+
+<html>
+    <head>
+        <style>
+            .ijo {
+                background-color: green;
+            }
+            .red {
+                background-color: red;
+            }
+        </style>
+    </head>
+    <body>
+    <table border="1" cellpadding="10" cellspacing="0" >
+        <?php for ($i=1; $i<=5; $i++) : ?>
+        <?php if($i % 2 == 1) :?>
+            <tr class="ijo">
+        <?php else : ?>
+        <tr class="red">
+        <?php endif; ?>
+            <?php for($j=1; $j<3; $j++):?>
+            <td><?="$i, $j"?></td>
+            <?php endfor;?>
+        </tr>
+        <?php endfor ; ?>
+    </table> 
+    </body>
+</html>
